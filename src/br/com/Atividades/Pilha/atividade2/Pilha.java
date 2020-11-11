39d09cdd1489a883672fa6c47307335cab764a67
lucas-cmh <lucas_santoscaminha@hotmail.com>
2020-11-10 22:28:11 -0300

    alteracao na logica da atividade 2 de pilhas

--- src/br/com/Atividades/Pilha/atividade2/Pilha.java
+++ src/br/com/Atividades/Pilha/atividade2/Pilha.java
@@ -8,53 +8,90 @@ package br.com.Atividades.Pilha.atividade2;
  * 
  */
 
+import java.text.Normalizer;
+
 public class Pilha {
-	
-	//private char[] ignore = {'-', ' ', '.', ','};
+
+	public static void main(String[] args) {
+
+		Pilha pilha = new Pilha();
+		pilha.palindromo("aibo-fobia");
+
+	}
+
+	private char[] ignore = {'-', ' ', '.', ','};
 	private char[] palavra;
 	private int posicaoPilha;
 	
 	// Construtor
-	public Pilha(String palavraInserida) {
-		this.palavra = new char[palavraInserida.length()]; 
-		
-		for(int i = 0; i < palavraInserida.length(); i++) {
-			this.palavra[i] = palavraInserida.charAt(i);
-		}
-		
-		this.posicaoPilha = palavraInserida.length() - 1;
+	public Pilha() {
+		this.posicaoPilha = -1;
 	}
 	
 	// Verifica se a palavra é um Palíndromo.
-	public boolean palindromo() {
-		
-		int percorrer = this.palavra.length;
-		int iguais = 0;
-		
-		System.out.println("Verificando Palíndromo...");
-		
-		for(int i = 0; i < this.palavra.length; i++) {	
-			
-			System.out.print(this.palavra[i] + " ");
-			if (this.palavra[i] == this.palavra[--percorrer]) {
-				iguais++;
-			}		
-									 
+	public void palindromo(String palavra) {
+		this.palavra = new char[palavra.length()];
+
+		for (int i = 0; i < palavra.length(); i++){
+			push(palavra.charAt(i));
 		}
-		
-		return iguais == this.palavra.length;
-	}
-	
 
-	public static void main(String[] args) {
-		
-		
-		Pilha pilha = new Pilha("aibofobia");
-		System.out.println("\n" + pilha.palindromo());
+		removePontuacaoOuEspacos(this.palavra);
 
-		
+		if (verificaPalindromo(this.palavra)){
+			System.out.println("A palavra " + palavra + " é um palíndromo.");
+		} else {
+			System.out.println("A palavra " + palavra + " NÃO é um palíndromo.");
+		}
 	}
-	
-	
+
+	private boolean verificaPalindromo(char[] palavra){
+
+		int percorrer = posicaoPilha;
+		int iguais = 0;
+
+		for (int i = 0; i < posicaoPilha; i++){
+			if (palavra[i] == palavra[percorrer--]){
+				iguais++;
+			}
+		}
+		return iguais == posicaoPilha;
+	}
+
+	public void push(char letra){
+		if (this.posicaoPilha < this.palavra.length - 1){
+			this.palavra[++posicaoPilha] = letra;
+		}
+	}
+
+	public char pop(){
+		if (isEmpty()) {
+			return ' ';
+		}
+		return this.palavra[this.posicaoPilha--];
+	}
+
+	// Verifica se a pilha está vazia.
+	public boolean isEmpty() {
+		if(this.posicaoPilha == -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Verifica se a pilha tem pontuação ou espaços
+	public void removePontuacaoOuEspacos(char[] palavra){
+
+		for (int i = 0; i < palavra.length; i++){
+			for (int j = 0; j < ignore.length; j++){
+				if (palavra[i] == ignore[j]){
+					//palavra[i] = palavra[i+1];
+				}
+			}
+		}
+
+
+	}
 
 }
